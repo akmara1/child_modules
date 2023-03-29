6387dbf74ef3b9ca397366f99b619ebc09fa1915
akmara1 <95084923+akmara1@users.noreply.github.com>
2023-03-29 23:25:50 +0600

    Update outputs.tf

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,10 +2,10 @@ output "vpc_id" {
   value       = aws_vpc.vpc.id
   description = "id of created vpc"
 }
-output "subnets_private" {
-  value       = aws_subnet.private[*].id
-  description = "ids of private subnets"
-}
+# output "subnets_private" {
+#   value       = aws_subnet.private[*].id
+#   description = "ids of private subnets"
+# }
 output "subnets_public" {
   value       = aws_subnet.public[*].id
   description = "ids of public subnets"
